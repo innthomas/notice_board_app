40e4309ac664f01059c41076c869ee137ccb90b2
Innocent Thomas <innthomas00158@gmail.com>
2020-10-11 11:11:41 +0100

    implementation of roster page pending

--- notice_board/lib/screens/duty_roster.dart
+++ notice_board/lib/screens/duty_roster.dart
@@ -4,6 +4,12 @@ class MyDutyRoster extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: Center(
+        child: Text(
+          "...to be implemented soon!",
+          style: TextStyle(color: Colors.red, fontSize: 20.0),
+        ),
+      ),
       color: Colors.green,
     );
   }
